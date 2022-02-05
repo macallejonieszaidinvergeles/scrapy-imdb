ff55460a7ee94a8d58645140867fbd1596680212
macallejonieszaidinvergeles <macallejon281@ieszaidinvergeles.org>
2022-02-05 02:57:18 +0100

    añade titulo y valoraciones, y ademas, salta a la siguiente pagina

--- scrapyTest/scrapyTest/spiders/__init__.py
+++ scrapyTest/scrapyTest/spiders/__init__.py
@@ -3,6 +3,9 @@ import scrapy
 from urllib.parse import urljoin
 # from flask import Request
 
+from random import randint
+from time import sleep
+
 
 class QuotesSpider(scrapy.Spider):
     name = "scrapyTest"
@@ -14,12 +17,13 @@ class QuotesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-
         # css ('div.content p :: text') Use las Herramientas para desarrolladores de Google Chrome para verificar la ubicación específica del texto que se debe rastrear
-        titulos = response.css('div.lister-item-content>h3.lister-item-header a::text').getall()
-        links = response.css('div.lister-item-content>h3.lister-item-header  a::attr(href)').extract()
+        titulos = response.css(
+            'div.lister-item-content>h3.lister-item-header a::text').getall()
+        links = response.css(
+            'div.lister-item-content>h3.lister-item-header  a::attr(href)').extract()
         # links = urljoin('https://www.imdb.com',links)
-        next = response.css('div.desc a::attr(href)').extract()
+        next = response.css('#main > div > div.desc > a.lister-page-next.next-page::attr(href)').extract()
         # next_page = response.css('.next a').attrib['href']
 
         links_completos = []
@@ -29,58 +33,43 @@ class QuotesSpider(scrapy.Spider):
             # print("----links----")
             links_completos.append(final_url)
 
-        base_url = "https://www.imdb.com"
-        next_final = urljoin(base_url, str(next[0]))
-        print("-------next--------")
-        print(next_final)
-        # print("linkk")
-        # print(links_completos[0])
-
-        # for link_completo in links_completos:
-        #  url = link_completo
-
-        dic = {}
-        dic = dict(zip(titulos, links_completos))
-
-        yield dic  # dic
-
-        # Lea el contenido de la página siguiente y vuelva a llamar al método de análisis
-        # for a in next_final:
-
-        
-        yield scrapy.Request(next_final, callback=self.title)
-
-
-    def title(self, response):
-        # css ('div.content p :: text') Use las Herramientas para desarrolladores de Google Chrome para verificar la ubicación específica del texto que se debe rastrear
-        titulos = response.css('div.lister-item-content>h3.lister-item-header a::text').getall()
-        links = response.css('div.lister-item-content>h3.lister-item-header  a::attr(href)').extract()
-        # links = urljoin('https://www.imdb.com',links)
-        next = response.css('div.desc a::attr(href)').extract()
-        # next_page = response.css('.next a').attrib['href']
-
-        links_completos = []
-        for link in links:
-            base_url = "https://www.imdb.com"
-            final_url = urljoin(base_url, link)
-            # print("----links----")
-            links_completos.append(final_url)
+        for link_completo in links_completos:
+            # print(link_completo)
+            yield scrapy.Request(link_completo, callback=self.reseñas)
 
         base_url = "https://www.imdb.com"
         next_final = urljoin(base_url, str(next[0]))
         print("-------next--------")
         print(next_final)
-        # print("linkk")
-        # print(links_completos[0])
 
-        # for link_completo in links_completos:
-        #  url = link_completo
+        if next:
+            yield scrapy.Request(next_final, callback=self.parse)
+            
 
-        dic = {}
-        dic = dict(zip(titulos, links_completos))
+    def reseñas(self, response):
+        # titulo = []
+        # reseñas_usuarios = []
+        # reseñas_criticos = []
+        # metapuntuación = []
 
-        yield dic  # dic
+        try:
+            titulo = response.css('div.jxsVNt h1::text').getall()
+            reseñas_usuarios = response.css(
+                'li.jxsVNt>a.ipc-link--baseAlt:link, .ipc-link--baseAlt>span.three-Elements span.score::text').getall()[0]
+            reseñas_criticos = response.css(
+                'li.jxsVNt>a.ipc-link--baseAlt:link, .ipc-link--baseAlt>span.three-Elements span.score::text').getall()[1]
+            metapuntuación = response.css(
+                'span.score-meta::text').getall()
 
-        
+        except IndexError:
+            titulo = "Not Available"
+            reseñas_usuarios = "Not Available"
+            reseñas_criticos = "Not Available"
+            metapuntuación = "Not Available"
 
-    
+        yield {
+            'titulo': titulo,
+            'reseñas_usuarios': reseñas_usuarios,
+            'reseñas_criticos': reseñas_criticos,
+            'metapuntuación': metapuntuación,
+        }
